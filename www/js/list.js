663d097dda7b458b027a3c7806f2fbc53d4c0a88
Richard Carey <richard.carey@icecreates.com>
2012-11-06 11:47:28 +0000

    v102

--- www/js/list.js
+++ www/js/list.js
@@ -22,7 +22,7 @@ function listInitialize() {
 
         for (var p = 0, propLength = listPoi.detailsPropertyOrder[key].length; p < propLength; p++) {
             var propKey = listPoi.detailsPropertyOrder[key];
-            $(".details" + i).append('<li><label>' + propKey[p] + ':</label><div class="details_info">' + listPoi.details[key][propKey[p]].replace("/n","<br />/n") + '</div></li>');
+            $(".details" + i).append('<li><label>' + propKey[p] + ':</label><div class="details_info">' + listPoi.details[key][propKey[p]].replace("/r","<br />/r") + '</div></li>');
         }
 
        
