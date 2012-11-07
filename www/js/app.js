28cde504d3138d811e7efc62fce8442f1e724a17
Richard Carey <richard.carey@icecreates.com>
2012-11-07 12:14:39 +0000

    v117

--- www/js/app.js
+++ www/js/app.js
@@ -51,215 +51,227 @@ var mapNewHeight;
 var navHeight;
 var lastScreenHeight;
 var lastScreenWidth;
-
+var resizeEnabled = true;
 
 function windowResize() {
-   
+    if (resizeEnabled) {
 
+        $("body").removeClass("landscape").removeClass("portrait");
+        $(".expander").hide();
+        $("#nav_toggle").hide();
+        $(".home_content").height("auto");
+        $('.nav li .icon img').height("auto").css("maxWidth", "auto").width("auto").css("maxHeight", "auto");
+        $('.nav li .label').height("auto");
+        $("#tabgroup img").height("auto").width("auto");
+        $("#tabgroup").width("1px").height("1px");
+        $(".page_html").width("auto");
+        $(".map_wrap").width("84%");
+        iconHeight = 0;
+        $(".home_content").css("marginTop", "0");
 
-    //screen sizes
-     screenHeight = screen.height;
-     windowWidth = window.innerWidth;
-     windowHeight = window.innerHeight;
-
-     
-
-         //     $('meta[name=viewport]').attr('content', 'width=' + windowWidth + ', initial-scale=1, maximum-scale=1, user-scalable=no, height=' + windowHeight + ', target-densityDpi=device-dpi');
-
-         //--orientation
-
-         if (windowWidth > windowHeight) {
-             $("body").addClass("landscape");
-             scaleMeasure = windowHeight;
-             $('meta[name=viewport]').attr('content', 'width=device-height, initial-scale=1.0, user-scalable=no, height=device-width, target-densityDpi=device-dpi');
-         } else {
-             $("body").addClass("portrait");
-             scaleMeasure = windowWidth;
-             $('meta[name=viewport]').attr('content', 'width=device-width, initial-scale=1.0, user-scalable=no, height=device-height, target-densityDpi=device-dpi');
-         }
-
-         //--font scale
-
-         if (scaleMeasure > 240) {
-             scale = scaleMeasure / 240;
-             fontSize = (12 * scale) + "px";
-
-             $("body").css("font-size", fontSize);
-         }
-
-
-         //get heights without icons
-         homeContentHeight = $(".home_content").outerHeight(true);
-         homeContentHeightDiff = windowHeight - homeContentHeight;
-
-         if (homeContentHeight < windowHeight) {
-
-             $(".home_content").height(homeContentHeight + homeContentHeightDiff);
-
-         }
-
-         //apply height differnece to box to expand down to bottom
-         expanderMargin = $(".nav ul").css("marginTop");
-
-         if ($("body").hasClass("content-page")) {
-             homeContentHeightDiff = windowHeight - $("#nhs_logo").height();
-             homeContentHeightDiff = homeContentHeightDiff - 40;
-             navHeight = $(".nav").height();
-             $(".home_content").css("marginTop", 0 - $(".home_content").height());
-
-         }
-
-         $(".expander").height(homeContentHeightDiff);
-         $(".expander").show();
-         //align labels
-         biggestHeight = 0;
-         $('.nav li .label').each(function (index) {
-             biggestHeight = Math.max(biggestHeight, $(this).outerHeight(true));
-         }).css("height", biggestHeight);
-         //resize icons
-
-         if ($("body").hasClass("portrait")) {
-             iconRows = 4;
-         } else {
-             iconRows = 2;
-         }
-         listAnchorHeight = homeContentHeightDiff / iconRows;
-         $('.nav li a').height(listAnchorHeight);
-
-         iconHeight = listAnchorHeight - biggestHeight;
-
-         $('.nav li .icon').height(Math.ceil(iconHeight));
-         $('#nav_toggle img').height(Math.ceil(iconHeight) / 2).css("marginTop", 0 - Math.ceil(iconHeight) / 4);
-         iconWidth = $(".nav ul li .icon").width();
-
-         if (iconWidth > iconHeight) {
-             $('.nav li .icon img').height(Math.ceil(iconHeight)).css("maxWidth", Math.ceil(iconWidth));
-         } else {
-             $('.nav li .icon img').width(Math.ceil(iconWidth)).css("maxheight", (iconHeight));
-         }
-         $("#nav_toggle").show();
-
-         if (windowWidth > windowHeight) {
-             nhsLogoHeight = $("#nhs_logo").height();
-             straplineHeight = $("#strapline").height();
-             straplineMargin = nhsLogoHeight - straplineHeight;
-             $("#strapline").css("margin-top", straplineMargin / 2);
-         };
-         //tabgroup calc
-         if ($("body").hasClass("portrait")) {
-             $('#tabgroup, #tabgroup img').height(Math.ceil(iconHeight));
-             $('#tabgroup').width(windowWidth * 0.9)
-         } else {
-             $('#tabgroup').width(windowWidth * 0.10).height(windowHeight - $("#nav_toggle").outerHeight(true));
-             $('#tabgroup img').width(windowWidth * 0.10)
-             //account for tabgroup width in content
-             $(".page_html").width(windowWidth * 0.88);
-             $(".map_wrap").width(windowWidth * 0.78);
-         }
-
-
-         //icon resize
-
-         //get high density 
-         dpr = 1;
-
-         if (window.devicePixelRatio !== undefined) {
-             dpr = window.devicePixelRatio;
-         }
-
-
-         absoluteImageSize = Math.ceil(iconHeight);
-
-         //calc which image to get
-         if (167 < absoluteImageSize) {
-             //ipad
-             imageSize = 284;
-         } else if (105 < absoluteImageSize && absoluteImageSize < 167) {
-             //s3
-             imageSize = 166;
-         } else if (65 < absoluteImageSize && absoluteImageSize < 105) {
-             //iphone4
-             imageSize = 105;
-         } else if (absoluteImageSize < 65) {
-             //smaller
-             imageSize = 65;
-         }
-         //if high density use double 
-         if (dpr > 1) {
-             imageSize = imageSize * 2;
-         }
-         //swap images
-
-
-         $('.iconSrcSwap').each(function (index) {
-             $(this).attr("src", $(this).attr("src").replace("/size/", "/" + imageSize + "/"));
-         });
-
-
-         //content page load
-
-         negativeHomeContentHeight = 0 - $(".home_content").outerHeight(true);
-         negativePageHeight = 0 - windowHeight;
-
-         navMarign = $(".nav").outerHeight(true) - $("#nav_toggle").outerHeight(true);
-
-         if ($("body").hasClass("content-page")) {
-
-
-             if ($("body").hasClass("show_nav")) {
-
-                 $(".nav").css("marginTop", 0);
-
-             } else {
-                 $(".nav").css("marginTop", 0);
-                 navMarign = $(".nav").outerHeight(true) - $("#nav_toggle").outerHeight(true);
-                 $(".nav").css("marginTop", 0 - navMarign);
-             }
-         }
+        //screen sizes
+        screenHeight = screen.height;
+        windowWidth = window.innerWidth;
+        windowHeight = window.innerHeight;
 
 
 
+        //     $('meta[name=viewport]').attr('content', 'width=' + windowWidth + ', initial-scale=1, maximum-scale=1, user-scalable=no, height=' + windowHeight + ', target-densityDpi=device-dpi');
 
-         //  map navigate
-         $(document).on("click", "#map a", function (e) {
-             e.preventDefault();
+        //--orientation
 
-             $(".page_html").fadeOut(400, function () {
-                 //            $.getScript("js/map.js");
-                 $(".map_wrap").fadeIn(400, function () {
-                     tabGroupinnerHeight = 10;
+        if (windowWidth > windowHeight) {
+            $("body").addClass("landscape");
+            scaleMeasure = windowHeight;
+            $('meta[name=viewport]').attr('content', 'width=device-height, initial-scale=1.0, user-scalable=no, height=device-width, target-densityDpi=device-dpi');
+        } else {
+            $("body").addClass("portrait");
+            scaleMeasure = windowWidth;
+            $('meta[name=viewport]').attr('content', 'width=device-width, initial-scale=1.0, user-scalable=no, height=device-height, target-densityDpi=device-dpi');
+        }
 
-                     if ($("body").hasClass("portrait")) {
-                         tabGroupinnerHeight = $("#tabgroup").innerHeight() + 20;
-                     }
-                     naviagtionToggleHeight = $("#nav_toggle").innerHeight();
-                     mapPageChrome = naviagtionToggleHeight + 20 + tabGroupinnerHeight;
+        //--font scale
 
-                     mapPaddingTop = $(".map_wrap").css("paddingTop");
-                     mapPadding = parseInt(mapPaddingTop) * 2;
-                     mapHeight = windowHeight - mapPageChrome;
-                     mapNewHeight = mapHeight - mapPadding;
+        if (scaleMeasure > 240) {
+            scale = scaleMeasure / 240;
+            fontSize = (12 * scale) + "px";
 
-                     $(".map_wrap").height(mapNewHeight);
-
+            $("body").css("font-size", fontSize);
+        }
 
 
-                     $('html, body').animate({ scrollTop: 0 }, 'slow');
-                     initialize();
-                     searchForm();
-                     //close menu if open
-                     if ($("#nav_toggle").hasClass("fixed_nav_toggle")) {
+        //get heights without icons
+        homeContentHeight = $(".home_content").outerHeight(true);
+        homeContentHeightDiff = windowHeight - homeContentHeight;
 
-                     } else {
-                         $("#nav_toggle").click();
-                     }
+        if (homeContentHeight < windowHeight) {
 
-                 });
-             });
+            $(".home_content").height(homeContentHeight + homeContentHeightDiff);
 
-         });
+        }
 
-    
+        //apply height differnece to box to expand down to bottom
+        expanderMargin = $(".nav ul").css("marginTop");
+
+        if ($("body").hasClass("content-page")) {
+            homeContentHeightDiff = windowHeight - $("#nhs_logo").height();
+            homeContentHeightDiff = homeContentHeightDiff - 40;
+            navHeight = $(".nav").height();
+            $(".home_content").css("marginTop", 0 - $(".home_content").height());
+
+        }
+
+        $(".expander").height(homeContentHeightDiff);
+        $(".expander").show();
+        //align labels
+        biggestHeight = 0;
+        $('.nav li .label').each(function (index) {
+            biggestHeight = Math.max(biggestHeight, $(this).outerHeight(true));
+        }).css("height", biggestHeight);
+        //resize icons
+
+        if ($("body").hasClass("portrait")) {
+            iconRows = 4;
+        } else {
+            iconRows = 2;
+        }
+        listAnchorHeight = homeContentHeightDiff / iconRows;
+        $('.nav li a').height(listAnchorHeight);
+
+        iconHeight = listAnchorHeight - biggestHeight;
+
+        $('.nav li .icon').height(Math.ceil(iconHeight));
+        $('#nav_toggle img').height(Math.ceil(iconHeight) / 2).css("marginTop", 0 - Math.ceil(iconHeight) / 4);
+        iconWidth = $(".nav ul li .icon").width();
+
+        if (iconWidth > iconHeight) {
+            $('.nav li .icon img').height(Math.ceil(iconHeight)).css("maxWidth", Math.ceil(iconWidth));
+        } else {
+            $('.nav li .icon img').width(Math.ceil(iconWidth)).css("maxheight", (iconHeight));
+        }
+        $("#nav_toggle").show();
+
+        if (windowWidth > windowHeight) {
+            nhsLogoHeight = $("#nhs_logo").height();
+            straplineHeight = $("#strapline").height();
+            straplineMargin = nhsLogoHeight - straplineHeight;
+            $("#strapline").css("margin-top", straplineMargin / 2);
+        };
+        //tabgroup calc
+        if ($("body").hasClass("portrait")) {
+            $('#tabgroup, #tabgroup img').height(Math.ceil(iconHeight));
+            $('#tabgroup').width(windowWidth * 0.9)
+        } else {
+            $('#tabgroup').width(windowWidth * 0.10).height(windowHeight - $("#nav_toggle").outerHeight(true));
+            $('#tabgroup img').width(windowWidth * 0.10)
+            //account for tabgroup width in content
+            $(".page_html").width(windowWidth * 0.88);
+            $(".map_wrap").width(windowWidth * 0.78);
+        }
+
+
+        //icon resize
+
+        //get high density 
+        dpr = 1;
+
+        if (window.devicePixelRatio !== undefined) {
+            dpr = window.devicePixelRatio;
+        }
+
+
+        absoluteImageSize = Math.ceil(iconHeight);
+
+        //calc which image to get
+        if (167 < absoluteImageSize) {
+            //ipad
+            imageSize = 284;
+        } else if (105 < absoluteImageSize && absoluteImageSize < 167) {
+            //s3
+            imageSize = 166;
+        } else if (65 < absoluteImageSize && absoluteImageSize < 105) {
+            //iphone4
+            imageSize = 105;
+        } else if (absoluteImageSize < 65) {
+            //smaller
+            imageSize = 65;
+        }
+        //if high density use double 
+        if (dpr > 1) {
+            imageSize = imageSize * 2;
+        }
+        //swap images
+
+
+        $('.iconSrcSwap').each(function (index) {
+            $(this).attr("src", $(this).attr("src").replace("/size/", "/" + imageSize + "/"));
+        });
+
+
+        //content page load
+
+        negativeHomeContentHeight = 0 - $(".home_content").outerHeight(true);
+        negativePageHeight = 0 - windowHeight;
+
+        navMarign = $(".nav").outerHeight(true) - $("#nav_toggle").outerHeight(true);
+
+        if ($("body").hasClass("content-page")) {
+
+
+            if ($("body").hasClass("show_nav")) {
+
+                $(".nav").css("marginTop", 0);
+
+            } else {
+                $(".nav").css("marginTop", 0);
+                navMarign = $(".nav").outerHeight(true) - $("#nav_toggle").outerHeight(true);
+                $(".nav").css("marginTop", 0 - navMarign);
+            }
+        }
+
+
+
+
+        //  map navigate
+        $(document).on("click", "#map a", function (e) {
+            e.preventDefault();
+
+            $(".page_html").fadeOut(400, function () {
+                //            $.getScript("js/map.js");
+                $(".map_wrap").fadeIn(400, function () {
+                    tabGroupinnerHeight = 10;
+
+                    if ($("body").hasClass("portrait")) {
+                        tabGroupinnerHeight = $("#tabgroup").innerHeight() + 20;
+                    }
+                    naviagtionToggleHeight = $("#nav_toggle").innerHeight();
+                    mapPageChrome = naviagtionToggleHeight + 20 + tabGroupinnerHeight;
+
+                    mapPaddingTop = $(".map_wrap").css("paddingTop");
+                    mapPadding = parseInt(mapPaddingTop) * 2;
+                    mapHeight = windowHeight - mapPageChrome;
+                    mapNewHeight = mapHeight - mapPadding;
+
+                    $(".map_wrap").height(mapNewHeight);
+
+
+
+                    $('html, body').animate({ scrollTop: 0 }, 'slow');
+                    initialize();
+                    searchForm();
+                    //close menu if open
+                    if ($("#nav_toggle").hasClass("fixed_nav_toggle")) {
+
+                    } else {
+                        $("#nav_toggle").click();
+                    }
+
+                });
+            });
+
+        });
+
+    }  
 };
 
 //NB - this needs to be bind so that the phone doesnt calculate the heights and such without images loaded.
@@ -383,58 +395,20 @@ $(document).ready(function () {
         $("body").removeClass("show_nav");
     });
 
-    $("#postocdeSearch").focus(function (e) {
-        e.preventDefault();
-        alert("on focus");
+    $("#postocdeSearch").focus(function () {
+        resizeEnabled = false;
     });
-    $("#postocdeSearch").blur(function (e) {
-        e.preventDefault();
-        alert("on blur");
+    $("#postocdeSearch").blur(function () {
+        resizeEnabled = true;
     });
 });
 
 $(window).resize(function () {
     waitForFinalEvent(function () {
 
-        
 
-        
-
-
-//        //check if width has changed if it hasnt its most likley the keyboard is on screen so we dont want to resize the viewport or it will mess up on android
-//        if (window.innerWidth == lastScreenWidth && window.innerHeight != lastScreenHeight) {
-//            //keep viewport the same instead of changing so the screen isnt resized.
-//            $('meta[name=viewport]').attr('content', 'width=' + lastScreenWidth + ', initial-scale=1.0, user-scalable=no, height=' + lastScreenHeight + ', target-densityDpi=device-dpi');
-//            alert("width hasntchanged");
-//            lastScreenHeight = window.innerHeight;
-//            lastScreenWidth = window.innerWidth;
-//     
-//        } else {
-            //reset before recalc
-            $("body").removeClass("landscape").removeClass("portrait");
-            $(".expander").hide();
-            $("#nav_toggle").hide();
-            $(".home_content").height("auto");
-            $('.nav li .icon img').height("auto").css("maxWidth", "auto").width("auto").css("maxHeight", "auto");
-            $('.nav li .label').height("auto");
-            $("#tabgroup img").height("auto").width("auto");
-            $("#tabgroup").width("1px").height("1px");
-            $(".page_html").width("auto");
-            $(".map_wrap").width("84%");
-            iconHeight = 0;
-            $(".home_content").css("marginTop", "0");
-
-
-           
-
-//            lastScreenHeight = windowHeight;
-//            lastScreenWidth = windowWidth;
 
             windowResize();
-
-//            alert("width changed");
-
-//        }
 
     }, 500, "1");
 });
@@ -445,42 +419,18 @@ function doOnOrientationChange() {
         case 90:
             waitForFinalEvent(function () {
 
-                $("body").removeClass("landscape").removeClass("portrait");
-                $(".expander").hide();
-                $("#nav_toggle").hide();
-                $(".home_content").height("auto");
-                $('.nav li .icon img').height("auto").css("maxWidth", "auto").width("auto").css("maxHeight", "auto");
-                $('.nav li .label').height("auto");
-                $("#tabgroup img").height("auto").width("auto");
-                $("#tabgroup").width("1px").height("1px");
-                $(".page_html").width("auto");
-                $(".map_wrap").width("84%");
-                iconHeight = 0;
-                //     $(".nav").css("marginTop", "0");
-                $(".home_content").css("marginTop", "0");
+               
                 windowResize();
-                alert("orienattion landscape");
+               
 
             }, 500, "1");
             break;
         default:
             waitForFinalEvent(function () {
 
-                $("body").removeClass("landscape").removeClass("portrait");
-                $(".expander").hide();
-                $("#nav_toggle").hide();
-                $(".home_content").height("auto");
-                $('.nav li .icon img').height("auto").css("maxWidth", "auto").width("auto").css("maxHeight", "auto");
-                $('.nav li .label').height("auto");
-                $("#tabgroup img").height("auto").width("auto");
-                $("#tabgroup").width("1px").height("1px");
-                $(".page_html").width("auto");
-                $(".map_wrap").width("84%");
-                iconHeight = 0;
-                //     $(".nav").css("marginTop", "0");
-                $(".home_content").css("marginTop", "0");
+              
                 windowResize();
-                alert("orienattion default");
+               
 
             }, 500, "1");
             break;
