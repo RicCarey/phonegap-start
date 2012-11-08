941f90056a157507c204d67ab46d4bac9be71488
Richard Carey <richard.carey@icecreates.com>
2012-11-08 10:50:57 +0000

    v129.5

--- www/js/map.js
+++ www/js/map.js
@@ -5,7 +5,7 @@ function initialize() {
     if (!window.searchResults){
         alert("An error has occurred attempting to load Google Maps, please try restarting the app.");
         window.jsonLocation = prompt("Enter your location");
-        $.getJSON("http://poi.nationalservers.co.uk/v1/search?format=json&key=nottingham-city-nhs&" + window.jsonLocation + "&callback=?&limit=15&type=" + window.poiType, function (data) {
+        $.getJSON("http://poi.nationalservers.co.uk/v1/search?format=json&key=nottingham-city-nhs&loc=" + window.jsonLocation + "&callback=?&limit=15&type=" + window.poiType, function (data) {
             window.searchResults = data;
         });
 //        if (!window.searchResults) {
